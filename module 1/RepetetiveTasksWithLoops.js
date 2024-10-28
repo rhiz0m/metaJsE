b67d4e21757ae40cc3d87b9b3272327fb4e8ecd7
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-10-28 14:33:00 +0100

    RepetetiveTasksWithLoops

---
+++ module 1/RepetetiveTasksWithLoops.js
@@ -0,0 +1,38 @@
+// RepetetiveTasksWithLoops
+
+// Task 1, 2. For Loops
+
+for (i = 1; i <= 5; i++) {
+  console.log(i)
+}
+console.log("Counting Completed!")
+
+for (i = 5; i > 0; i--) {
+  console.log(i)
+}
+console.log("Countdown finished!")
+
+// Task 3, 4, 5. While Loops
+
+var i = 1
+
+while (i <= 5) {
+  console.log(i)
+  i += 1
+}
+console.log("Counting completed!")
+
+var j = 5
+
+while (j > 0) {
+  console.log(j)
+  j -= 1
+}
+console.log("Countdown finished")
+
+var year = 2018
+
+while (year < 2023) {
+  console.log(year)
+  year += 1
+}
