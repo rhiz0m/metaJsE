15cfd8a9c35b4515501757228f5137055db075a4
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-10-28 15:57:59 +0100

    Nested Loops

---
+++ module 1/NestedLoops.js
@@ -0,0 +1,17 @@
+// Nested Loops
+
+for (year = 2022; year <= 2024; year++) {
+  console.log(`The year: ${year}`)
+  for (month = 6; month <= 9; month++) {
+    console.log(`-- The Summer month's of the year: ${month}`)
+  }
+}
+
+// Calculator
+for (num1 = 0; num1 <= 9; num1++) {
+  for (num2 = 0; num2 <= 10; num2++) {
+    console.log(`${num1} x ${num2} equals ${num1 * num2}`)
+  }
+  console.log("-----------------------")
+}
+
