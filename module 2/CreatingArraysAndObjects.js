07444588a1e796b175b4184360eeef9091469b59
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-10-29 20:02:06 +0100

    Creating Arrays and Objects

---
+++ module 2/CreatingArraysAndObjects.js
@@ -0,0 +1,28 @@
+// Creating Arrays and Objects
+
+// Populating Arrays
+let clothes = []
+
+// Add clothes
+clothes.push("cap", "Tshirt", "Jeans", "sneakers", "belt")
+console.log(clothes)
+
+// Remove
+clothes.pop()
+console.log(clothes)
+
+clothes.push("socks")
+console.log(clothes)
+
+// Show the third item in the array
+console.log(`The third item in the array is: ${clothes[2]}`)
+
+// Populate an Object
+let favCar = {}
+
+favCar.color = "black"
+console.log(favCar)
+
+favCar.covertible = true
+
+console.log(favCar)
