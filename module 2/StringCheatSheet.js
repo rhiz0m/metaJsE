42add4ae4923bb7c8562855a380175766a566e30
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-10-29 19:39:11 +0100

    JS - String Cheat Sheet

---
+++ module 2/StringCheatSheet.js
@@ -0,0 +1,41 @@
+// String Cheat Sheet
+
+var greet = "Hello, "
+var place = "World"
+
+greet.length // 7
+greet.charAt(0) // 'H'
+
+// Concat
+console.log("He".concat("ll".concat("o")))
+
+// The lastIndexOf finds the last match, otherwise it works the same as indexOf.
+"ho-ho-ho".indexOf("h") // 0
+"ho-ho-ho".indexOf("o") // 1
+"ho-ho-ho".indexOf("-") // 2
+
+"ho-ho-ho".split("-") // ['ho', 'ho', 'ho']
+console.log("Andreas".split("d")) // [ 'An', 'reas' ]
+
+greet.toUpperCase() // "HELLO, "
+greet.toLowerCase() // "hello, "
+
+/** List 
+ * 
+charAt() 
+
+concat() 
+
+indexOf() 
+
+lastIndexOf() 
+
+split() 
+
+toUpperCase() 
+
+toLowerCase()  
+ * 
+ * 
+ * 
+ */
