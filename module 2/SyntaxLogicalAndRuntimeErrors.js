bd02c1259150f0cf1eac30254fbd792408b84b2b
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-10-30 12:58:28 +0100

    Exercise error prevention

--- module 2/SyntaxLogicalAndRuntimeErrors.js
+++ module 2/SyntaxLogicalAndRuntimeErrors.js
@@ -26,13 +26,17 @@ console.log(username)
 // Uncaught ReferenceError: username is not defined
 
 // Uncaught SyntaxError: Unexpected string
-var a "there's no assignment operator here";
+
+// var a "there's no assignment operator here";
 
 // Strings don't have the array methods aviable to them
-"hello".pop() // Uncaught TypeError: "hello".pop is not a function
+"hello"
+  .pop()
+  (
+    // Uncaught TypeError: "hello".pop is not a function
 
-// Range Error
-/**
+    // Range Error
+    /**
  * Example with 10
 Divide 10 by 2. The quotient is 5, remainder 0.
 Divide 5 by 2. The quotient is 2, remainder 1.
@@ -40,7 +44,9 @@ Divide 2 by 2. The quotient is 1, remainder 0.
 Divide 1 by 2. The quotient is 0, remainder 1.
 Now, read the remainders from bottom to top, giving 1010.
  */
-(10).toString(2); // '1010'
+    10
+  )
+  .toString(2) // '1010'
 
 // Example of out of Range
-(10).toString(100); // Uncaught RangeError: toString() radix argument must be between 2 and 36
+;(10).toString(100) // Uncaught RangeError: toString() radix argument must be between 2 and 36
