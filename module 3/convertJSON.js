c075aa2a9f0a9b84191e838776490f95cbea81a0
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-11-03 12:57:10 +0100

    convert JSON to Object and back

---
+++ module 3/convertJSON.js
@@ -0,0 +1,23 @@
+// Convert a JSON string to an object ->
+
+const jsonStr = '{"greeting" : "hello"}'
+JSON.parse(jsonStr)
+
+//console.log(jsonStr) // {"greeting" : "hello"}
+
+const aPlainObj = JSON.parse(jsonStr)
+aPlainObj.greeting = "hi"
+
+//console.log(aPlainObj) // { greeting: 'hi' }
+
+// convert an object to a JSON string ->
+
+const data = {
+  firstName: "John",
+  lastName: "Doe",
+  greeting: "Hello!",
+}
+
+//console.log(data) // { firstName: 'John', lastName: 'Doe', greeting: 'Hello!' }
+
+console.log(JSON.stringify(data)) // {"firstName":"John","lastName":"Doe","greeting":"Hello!"}
