105abb9fa51072059612f553bc428c1ba8975784
Andreas Antonsson <antonsson.andreas@hotmail.com>
2024-11-05 18:39:35 +0100

    JS Programming Final Project

--- module 5/FinalProject/finalProject.js
+++ module 5/FinalProject/finalProject.js
@@ -20,9 +20,43 @@ const dishData = [
 const tax = 1.2
 
 // Implement getPrices()
-function getPrices() {}
+function getPrices(taxBoolean) {
+  for (const dish of dishData) {
+    let finalPrice
+    if (taxBoolean === true) {
+      finalPrice = dish.price * tax
+    } else if (taxBoolean === false) {
+      finalPrice = dish.price
+    } else {
+      console.log("You need to pass a boolean to the getPrices call!")
+      return
+    }
+    console.log(`Dish: ${dish.name} Price: $${finalPrice}`)
+  }
+}
 
 // Implement getDiscount()
-function getDiscount() {}
+function getDiscount(taxBoolean, guests) {
+  getPrices(taxBoolean)
+
+  if (typeof guests === "number" && guests > 0 && guests < 30) {
+    let discount = 0
+    if (guests < 5) {
+      discount = 5
+    } else if (guests >= 5) {
+      discount = 10
+    }
+    console.log(`Discount is: $${discount}`)
+  } else {
+    console.log("The second argument must be a number between 0 and 30")
+  }
+}
 
 // Call getDiscount()
+getDiscount(false, 2)
+getDiscount(true, 800)
+
+function addTwo(a, b) {
+  return a
+}
+console.log(addTwo(5, 10))
